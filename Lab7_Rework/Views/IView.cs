5dd34c0e57d1d119a71ab369e14af0826de7c837
IvanTheFox <ivan.shepelev.06@inbox.ru>
2026-05-27 14:50:23 +0300

    first commit

--- Lab7_Rework/Views/IView.cs
+++ Lab7_Rework/Views/IView.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Lab7_Rework.Model;
 
 namespace Lab7_Rework.Views
 {
     internal interface IView
     {
-        
+        void AddTrain(Train train);
+        void RemoveTrain(int id);
+        void ModifyTrain(Train train);
+
+        void OnTrainAdded(Train train);
+        void OnTrainRemoved(int id);
+        void OnTrainModified(Train train);
     }
 }
