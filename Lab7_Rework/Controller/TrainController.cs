9696d43e8b614a06cd09a9884dcbdf4f4fed9df8
Vlad <vladgrid06@mail.ru>
2026-05-28 12:49:24 +0300

    Добавлена проверка параметров и вызов конструктора в контроллере

--- Lab7_Rework/Controller/TrainController.cs
+++ Lab7_Rework/Controller/TrainController.cs
@@ -30,9 +30,14 @@ namespace Lab7_Rework.Controller
         }
 
         /// <summary>Добавить новый поезд из данных представления</summary>
-        public void Add(string number, string departure, Time time, Train.TrainType type, int seatsTotal, int seatsAvailable)
+        public void Add(int id, string number, string departure, Time time, Train.TrainType type, int seatsTotal, int seatsAvailable)
         {
-            // Проверка на здравый смысл переданных параметров
+            // Проверка переданных параметров
+            if (seatsAvailable > seatsTotal)
+            {
+                throw new ArgumentException("Свободных мест не может быть больше общего количества мест");
+            }
+            Train train = new Train(id, number, departure, time, type, seatsTotal, seatsAvailable);
             _model.AddTrain(train);
         }
 
